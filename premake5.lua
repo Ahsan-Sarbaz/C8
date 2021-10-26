02e6a3269ac9fea91f0cd755821911891a39c641
Ahsan-Sarbaz <geeksdotpk@gmail.com>
2021-10-26 15:24:26 +0500

    added premake

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+-- premake5.lua
+workspace "CHIP8"
+    configurations { "Debug", "Release" }
+    platforms {"X64"}
+
+project "CHIP8"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "build/%{cfg.buildcfg}"
+
+    files { "src/**.h", "src/**.cpp" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
